8193de2d1e7881626cdc4cf9c02783a4a47d0976
Visfxb <evermir7@gmail.com>
2025-01-26 22:21:50 +0200

    Created main and array workers

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,1 +1,12 @@
-﻿#include <iostream>
+﻿#include "Employ.h"
+
+int main()
+{
+    Employ workers[5] = {
+        {"Surname_1", 1990, "Manager_1", 25000, "1"},
+        {"Surname_2", 1970, "Worker_1", 30000, "2"},
+        {"Surname_3", 1950, "Manager_2", 35000, "3"},
+        {"Surname_4", 1930, "Worker_2", 40000, "4"},
+        {"Surname_5", 1910, "Manager_3", 45000, "5"}
+    };
+}
